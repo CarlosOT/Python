9b34ebf5af3b1bb79cc3e13682d27a519b48a6cb
Carlos Teixeira <39592947+CarlosOT@users.noreply.github.com>
2018-07-09 17:52:27 -0300

    Update lambdaConcatenation.py

--- lambdaConcatenation.py
+++ lambdaConcatenation.py
@@ -4,3 +4,17 @@ lastName = concatenation(' Teixeira')
 FullName = lastName('Carlos')
 
 print(FullName)
+
+
+----------
+
+
+def fullName():
+	name = lambda name, lastName : name + " " + lastName
+	return name
+
+myName = fullName()
+
+print(myName('Carlos','Teixeira'))
+
+
