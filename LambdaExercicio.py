b10600714bda1d12bd3d54e6429a9d80e4840129
Carlos Teixeira <39592947+CarlosOT@users.noreply.github.com>
2018-07-09 19:11:18 -0300

    Update LambdaExercicio.py

--- LambdaExercicio.py
+++ LambdaExercicio.py
@@ -1,3 +1,5 @@
+# meu código
+
 from time import sleep
 
 def somandoNumeros(): return lambda valor1, valor2 : valor1 + valor2
@@ -9,7 +11,7 @@ def multiplicaoNumeros(): return lambda valor1, valor2 : valor1 * valor2
 def divisaoNumeros(): return lambda valor1, valor2 : valor1 / valor2
 
 
-opcao = int(input('Escolha uma das opções operações: 1(+), 2(-), 3(*), 4(/), Sair(0) '))
+opcao = int(input('Escolha uma das operações: 1(+), 2(-), 3(*), 4(/), Sair(0) '))
 
 if opcao == 0:
 	print('Finalizando programa ... ')
@@ -43,6 +45,35 @@ elif opcao == 4:
 	dividindo = divisaoNumeros()
 	resultado = dividindo(primeiroValor, segundoValor)
 	print(f"A divisão de {primeiroValor} por {segundoValor} é {resultado:.2f}")
+	
+	
+---------
+
+
+# código do professor
+
+soma = lambda x, y: x + y
+subtracao = lambda x, y: x - y
+multiplicacao = lambda x, y: x * y
+divisao = lambda x, y: x / y
+
+operacao = int(input(" 1: Soma\n 2: Subtração\n 3: Multiplicação\n 4: Divisão\n Informe o número da operação: "))
+if operacao not in [1, 2, 3, 4]:
+    print("Operação inválida.")
+    exit()
+
+numero1 = int(input("Informe o primeiro número: "))
+numero2 = int(input("Informe o segundo número: "))
+
+if operacao == 1:
+    print("Resultado da soma: ", soma(numero1, numero2))
+elif operacao == 2:
+    print("Resultado da subtração: ", subtracao(numero1, numero2))
+elif operacao == 3:
+    print("Resultado da multiplicação: ", multiplicacao(numero1, numero2))
+else:
+    print("Resultado da divisão: ", divisao(numero1, numero2))
+
 
 
 
