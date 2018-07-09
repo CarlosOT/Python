d6867cf88dba0327427648d02a5fd2357a4710bf
Carlos Teixeira <39592947+CarlosOT@users.noreply.github.com>
2018-07-09 17:40:29 -0300

    Update lambdaList.py

--- lambdaList.py
+++ lambdaList.py
@@ -3,3 +3,23 @@ MakingSum = lambda numberList : sum(numberList)
 numberList = [1,2,3,4,5,6,7,8,9,10]
 Result = MakingSum(numberList)
 print(Result)
+
+
+-----------
+
+
+higherNumber = lambda numberList : max(numberList)
+
+numberList = [12,45,32,45,67,54,67,48,76,78,90,4]
+Result = higherNumber(numberList)
+print(Result)
+
+
+------------
+
+
+smallerNumber = lambda listNumber : min(listNumber)
+
+listNumber = [67,85,743,34,563,546,2324,675,56]
+result = smallerNumber(listNumber)
+print(result)
