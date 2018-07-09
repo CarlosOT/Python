dee8efdfd6a9c1b59b8a145bcf49924e66d0b924
Carlos Teixeira <39592947+CarlosOT@users.noreply.github.com>
2018-07-09 16:54:48 -0300

    Update lambdaMultiplication.py
    adding more things

--- lambdaMultiplication.py
+++ lambdaMultiplication.py
@@ -4,3 +4,12 @@ UserInput = int(input('Inser a value: '))
 Value = FunctionLambda(UserInput)
 
 print(f"{UserInput} multiplied by 2 is {Value}")
+
+----------------------------
+
+def multiplicator(n): return lambda item : item * n
+
+value = multiplicator(3)
+resultado = value(3)
+
+print(resultado)
