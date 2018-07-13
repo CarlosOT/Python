76a476d3f5f5ccf08911925bc780bb18d9750db3
Carlos Teixeira <39592947+CarlosOT@users.noreply.github.com>
2018-07-12 21:06:43 -0300

    Create list_starts_with_C.py

---
+++ list_starts_with_C.py
@@ -0,0 +1,6 @@
+lista_nomes = ['carlos','fernando','carol','felipe','catarina']
+
+for nomes in lista_nomes:
+	nomes_capitalize = nomes.capitalize()
+	if nomes_capitalize.startswith('C'):
+		print(nomes_capitalize)
