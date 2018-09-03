ee7344f06d4d8619d0612fc8e3793a113b195f2f
Carlos Teixeira <39592947+CarlosOT@users.noreply.github.com>
2018-09-02 21:56:17 -0300

    Create classePessoa.py

---
+++ AC_LinguagemProgramacao/classePessoa.py
@@ -0,0 +1,6 @@
+class Pessoa:
+    def __init__(self,nome, sobrenome, dataNasc, nacionalidade):
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.dataNasc = dataNasc
+        self.nacionalidade = nacionalidade
