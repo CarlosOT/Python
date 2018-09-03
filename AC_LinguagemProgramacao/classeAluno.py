0ea6b3572ade8b8f9c5960222d1e55aa709a4526
Carlos Teixeira <39592947+CarlosOT@users.noreply.github.com>
2018-09-02 21:55:47 -0300

    Create classeAluno.py

---
+++ AC_LinguagemProgramacao/classeAluno.py
@@ -0,0 +1,90 @@
+from classePessoa import Pessoa
+
+class Aluno(Pessoa):
+
+    def __init__(self,nome, sobrenome, dataNasc, nacionalidade, RA, curso,notaComportamento, frequencia):
+        Pessoa.__init__(self,nome,sobrenome,dataNasc,nacionalidade)
+        self.RA = RA
+        self.curso = curso
+        self.materia = ''
+        self.notaComportamento = notaComportamento
+        self.frequencia = frequencia
+        self.listaNotas = []
+        self.media = ''
+        self.boletim = {}
+
+    def LancarNotasAluno(self):
+
+        validacaoDisciplina = True
+        while validacaoDisciplina:
+            try:
+                disciplinas = int(input('\nQuantidade de Disciplinas a cadastrar: ')) 
+                validacaoDisciplina = False 
+            except:
+                print('opção inválida')
+
+        validacaoQtdeNotas = True
+        while validacaoQtdeNotas:
+            try:
+                qtde_notas = int(input('Quantidade de notas a cadastrar: '))
+                validacaoQtdeNotas = False
+            except:
+                print('opção inválida')
+
+        for iteracao_discplina in range(disciplinas):
+
+            self.materia = input('\nMatéria: ')
+
+            contador = 0
+            while contador < qtde_notas:
+                try:
+                    nota = int(input('nota: '))
+                except:
+                    print('opção inválida')
+                    continue
+
+                if nota < 0 or nota > 10:
+                    print('opção inválida')
+                    continue
+                else:
+                    self.listaNotas.append(nota)
+                    contador += 1
+
+                def mediaAluno():
+                    soma = sum(self.listaNotas)
+                    elementos_lista = len(self.listaNotas)
+                    media = soma / elementos_lista
+                    self.media = media
+                mediaAluno()
+
+                self.boletim[self.materia] = self.media
+
+    def imprimeBoletim(self):
+        print('\nMatérias / Médias\n')
+        for key, value in self.boletim.items():
+            print(f'{key} - {value:.2f}')
+
+    def imprimeDadosAluno(self):
+        print(f'\nNome: {self.nome} {self.sobrenome} / Nascimento: {self.dataNasc} / {self.nacionalidade}')
+        print(f'Curso: {self.curso} / RA: {self.RA} / Nota Comportamento: {self.notaComportamento} / Frequência: {self.frequencia}%')
+                    
+    
+
+'''
+
+nome = input('\nNome do aluno: ')
+sobrenome = input('Sobrenome: ')
+data_nascimento = input('Data nascimento: ')
+Nacionalidade = input('Nacionalidade: ')
+RA = input('RA: ')
+curso = input('Curso: ')
+notaComportamento = input('Nota comportamento: ')
+frequencia = input('Frequência: ')
+
+
+x = Aluno(nome, sobrenome, data_nascimento, Nacionalidade, RA, curso,notaComportamento, frequencia)
+x.LancarNotasAluno()
+x.imprimeDadosAluno()
+x.imprimeBoletim() 
+
+'''
