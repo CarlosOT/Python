004e89f9b1086fe7460f19027075d9986efabc71
carlosot <c.texeirajr@gmail.com>
2018-09-05 19:22:48 -0300

    organizing

---
+++ scripts/script_search.py
@@ -0,0 +1,27 @@
+import os
+import shutil
+
+def readFile(directory, ContentSearch):
+	os.chdir('path') # ex. '/home/exemplo/Desktop/pastaPrincipal'
+	ListFile = os.listdir(directory)
+	os.chdir('./' + directory) # ex. '/home/exemplo/Desktop/pastaPrincipal/arquivo'
+	for file in ListFile:
+		f = open(file)
+		content = f.read()
+		if ContentSearch in content:
+			shutil.copy(file, 'path' ) # ex. '/home/exemplo/Desktop/pastaPrincipal'
+			f.close()
+			exit()
+
+os.chdir('path') # ex. '/home/exemplo/Desktop'
+folder = input('Nome da pasta principal que contém as pastas com os arquivos: ')
+directories = os.listdir(folder)
+ContentSearch = input('Conteúdo a ser procurado: ')
+
+for directory in directories:
+	readFile(directory, ContentSearch)
+
+
+
+
+
