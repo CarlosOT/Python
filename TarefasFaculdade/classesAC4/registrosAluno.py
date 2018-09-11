936a3a1534105734ebcec73f7d678763a7c2d536
Carlos Teixeira <39592947+CarlosOT@users.noreply.github.com>
2018-09-11 20:01:47 -0300

    Update registrosAluno.py

--- TarefasFaculdade/classesAC4/registrosAluno.py
+++ TarefasFaculdade/classesAC4/registrosAluno.py
@@ -1,9 +1,10 @@
-from allClasses import *
+from allClasses import * 
 
 print('\n*Os dados do aluno serão salvos em um arquivo\n')
 
 while True:
-    while True: # validando nome
+    #VALIDACOES ALUNO
+    while True: #nome
         try:
             nome = input('\nNome: ').capitalize()
             if nome == '':
@@ -14,14 +15,14 @@ while True:
             continue
         except:
             break
-    while True: # validando endereço
+    while True: #endereço
         endereco = input('Endereço: ').capitalize()
         if len(endereco) == 0:
             print('\n!!! esse campo não pode ser nulo !!!\n')
             continue
         else:
             break
-    while True: # validando data de nascimento
+    while True: #data de nascimento
         dataNascimento = input('Data de nascimento (ex.10-06-1996): ')
         if dataNascimento == '':
             print('\n!!! esse campo não pode ser nulo !!!\n')
@@ -55,7 +56,7 @@ while True:
             continue
         else:
             break
-    while True:  # validando sexo
+    while True: #sexo
         sexo = input('Sexo [M] ou [F]: ').upper()
         if sexo == 'X':
             exit()
@@ -67,7 +68,7 @@ while True:
         else:
             print('\n!!! Valor Inválido !!!\n')
             continue
-    while True: # validando telefone celular
+    while True: #telefone celular
         telefoneCelular = input('Telefone Celular (ex.11 97865-8909): ')
         if telefoneCelular == '':
             print('\n!!! esse campo não pode ser nulo !!!\n')
@@ -97,7 +98,7 @@ while True:
         else:
             print('\n!!! Valor Inválido !!!\n')
             continue
-    while True: # validando telefone residencial
+    while True: #telefone residencial
         telefoneResidencial = input('Telefone Residencial (ex.11 3456-8909): ')
         if telefoneResidencial == '':
             print('\n!!! esse campo não pode ser nulo !!!\n')
@@ -127,7 +128,7 @@ while True:
         else:
             print('\n!!! Valor Inválido !!!\n')
             continue
-    while True: # validando o e-mail
+    while True: #e-mail
         email = input('Email: ').lower().replace(' ','')
         if len(email) == 0:
             print('\n!!! esse campo não pode ser nulo !!!\n')
@@ -153,7 +154,7 @@ while True:
             continue
         else:
             break
-    while True: # validando o estado civil
+    while True: #estado civil
         listaCivil = ['SOLTEIRO','CASADO','DIVORCIADO','VIÚVO']
         estadoCivil = input('Estado Civil: ').upper().replace(' ','')
         if estadoCivil == 'VIUVO':
@@ -167,9 +168,8 @@ while True:
             continue
         else:
             estadoCivil = estadoCivil.capitalize()
-            print(estadoCivil)
             break
-    while True: # validando RA
+    while True: #  RA
         ra = input('Ra: ')
         if len(ra) == 0:
             print('\n!!! esse campo não pode ser nulo !!!\n')
@@ -179,7 +179,154 @@ while True:
         else:
             print('\n!!! Valor Inválido, o RA precisa ser um sequência de 7 números!!!\n')
             continue
-    while True: # validando nome do arquivo
+    dadosAluno = Aluno(nome,endereco,dataNascimento,sexo,telefoneCelular,telefoneResidencial,email,estadoCivil,ra)
+    # VALIDACOES CURSO
+    while True: #nome curso
+        try:
+            nome = input('\nCurso: ').capitalize()
+            if nome == '':
+                print('\n!!! esse campo não pode ser nulo !!!\n')
+                continue
+            validaString = int(nome)
+            print('\n!!! Valor Inválido !!!\n')
+            continue
+        except:
+            break
+    while True: #periodo
+        listaPeriodo = ['diurno','noturno','matinal']
+        periodo = input('Periodo: ').lower().replace(' ','')
+        if periodo == 'x':
+            exit()
+        if len(periodo) == 0:
+            print('\n!!! esse campo não pode ser nulo !!!\n')
+            continue
+        if periodo in listaPeriodo:
+            periodo = periodo.capitalize()
+            break
+        else:
+            print('\n!!! Inválido !!!\n')
+            print('\n*opções disponíveis: Diurno, Noturno e Matinal\n')
+            continue
+    while True: #duracao
+        duracao = input('Quantidade de semestres: ')
+        if len(duracao) == 0:
+            print('\n!!! esse campo não pode ser nulo !!!\n')
+            continue
+        if int(duracao) == 1:
+            duracao = f'{duracao} Semestre'
+            break
+        elif int(duracao) > 1:
+            duracao = f'{duracao} Semestres'
+            break
+        else:
+            print('\n!!! Valor Inválido !!!\n')
+            continue   
+    while True: #notaMEC
+        notaMEC = input('Nota MEC: ')
+        if len(notaMEC) == 0:
+            print('\n!!! esse campo não pode ser nulo !!!\n')
+            continue
+        if notaMEC.isdecimal() == True and int(notaMEC) >= 0 and int(notaMEC) <= 10:
+            break
+        else:
+            print('\n!!! Valor Inválido !!!\n')
+            continue
+    while True: #valorMensalidade
+        try:
+            valorMensalidade = float(input('Valor Mensalidade: '))
+        except ValueError:
+            print('\n!!! Inválido, informe um número (exemplo 100 ou 250.00) !!!\n')
+        else:
+            valorMensalidade = f'R${valorMensalidade:.2f}'
+            print(valorMensalidade)
+            break
+    while True: #tipo
+        try:
+            tipo = input('\nTipo: ').capitalize().replace(' ','')
+            if tipo == '':
+                print('\n!!! esse campo não pode ser nulo !!!\n')
+                continue
+            validaString = int(tipo)
+            print('\n!!! Valor Inválido !!!\n')
+            continue
+        except:
+            break
+    while True: #descricao
+        try:
+            descricao = input('\nDescrição: ')
+            if descricao == '':
+                print('\n!!! esse campo não pode ser nulo !!!\n')
+                continue
+            if len(descricao) > 100:
+                print('\n!!! A descrição precisa ter no máximo 100 caracteres !!!\n')
+                continue
+            validaString = int(descricao)
+            print('\n!!! Valor Inválido !!!\n')
+            continue
+        except:
+            break
+    while True: #disciplinas
+        try:
+            disciplinas = input('\nDisciplinas: ')
+            if descricao == '':
+                print('\n!!! esse campo não pode ser nulo !!!\n')
+                continue
+            if len(disciplinas) > 100:
+                print('\n!!! Esse campo pode ter no máximo 100 caracteres !!!\n')
+                continue
+            validaString = int(disciplinas)
+            print('\n!!! Valor Inválido !!!\n')
+            continue
+        except:
+            break
+    dadosCurso = Curso(nome, periodo, duracao, notaMEC, valorMensalidade, tipo, descricao, disciplinas)
+    # VALIDACOES MATRÍCULA
+    while True: #número matrícula
+        numeroMatricula = input('Número Matrícula: ')
+        if len(numeroMatricula) == 0:
+            print('\n!!! esse campo não pode ser nulo !!!\n')
+            continue
+        if numeroMatricula.isdecimal() == True and len(numeroMatricula) == 10:
+            break
+        else:
+            print('\n!!! Valor Inválido, o número da matrícula precisa ser um sequência de 10 números!!!\n')
+            continue
+    while True: #data
+        dataMatricula = input('Data Matrícula (ex.14-07-1966): ')
+        if dataMatricula == '':
+            print('\n!!! esse campo não pode ser nulo !!!\n')
+            continue
+        if len(dataMatricula) != 10:
+            print('\n!!! Valor Inválido !!!\n')
+            continue
+        contaHifen = dataMatricula.count('-')
+        if contaHifen != 2:
+            print('\n!!! Valor Inválido !!!\n')
+            continue
+        validaDataMatricula = dataMatricula.split('-')
+        if len(validaDataMatricula) != 3:
+            print('\n!!! Valor Inválido !!!\n')
+            continue
+        listaTamanho = []
+        for i in validaDataMatricula:
+            tamanhoElemento = len(i)
+            listaTamanho.append(tamanhoElemento)
+        if listaTamanho != [2,2,4]:
+            print('\n!!! Valor Inválido !!!\n')
+            continue
+        contaErros = 0
+        for i in validaDataMatricula:
+            try:
+                validaNumero = int(i)
+            except:
+                contaErros += 1
+        if contaErros != 0:
+            print('\n!!! Valor Inválido !!!\n')
+            continue
+        else:
+            break
+    dadosMatricula = Matricula(numeroMatricula,dataMatricula)
+    while True: #nome do arquivo
             nomeArquivo = input('\nDefina o nome do arquivo onde os dados serão salvos: ')
             if len(nomeArquivo) == 0:
                 print('\n!!! esse campo não pode ser nulo !!!\n')
@@ -190,14 +337,9 @@ while True:
             else:
                 print('\n!!! O arquivo precisa ter uma extensão .txt !!!\n')
                 continue
-
-    dadosAluno = Aluno(nome,endereco,dataNascimento,sexo,telefoneCelular,telefoneResidencial,email,estadoCivil,ra)
-    dadosCurso = Curso('ADS','Noturno','2','10','500,00','Tecnólogo','Curso voltado para ...','Devops, banco de dados, python ...')
-    dadosMatricula = Matricula('64758475796','10-04-2016')
     y = RegistrosAluno(dadosAluno,dadosCurso,dadosMatricula, nomeArquivo)
     y.salvarRegistrosAluno()
-
-    while True: # repete a função validaDadosAluno
+    while True: #repete a função validaDadosAluno
             repete = input('\nPara salvar dados de mais um aluno digite "1" ou "2" para sair\n')
             if len(repete) == 0:
                 print('\n!!! esse campo não pode ser nulo !!!\n')
@@ -212,6 +354,3 @@ while True:
             else:
                 print('\n!!! Inválido !!!\n')
                 continue
-
-
-
