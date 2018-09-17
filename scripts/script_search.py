98b5b58881751388dad482718d9c2c84e92026bf
Carlos Teixeira <39592947+CarlosOT@users.noreply.github.com>
2018-09-17 17:29:13 -0300

    Procurando conteúdo em arquivos com python
    Procurando conteúdo em arquivos com python

--- scripts/script_search.py
+++ scripts/script_search.py
@@ -1,25 +1,45 @@
 import os
 import shutil
 
-def readFile(directory, ContentSearch):
-	os.chdir('path') # ex. '/home/exemplo/Desktop/pastaPrincipal'
+def readFile(PrincipalFolder, directory, ContentSearch, destination):
+	os.chdir('/home/carlos/Desktop/'+ PrincipalFolder)
 	ListFile = os.listdir(directory)
-	os.chdir('./' + directory) # ex. '/home/exemplo/Desktop/pastaPrincipal/arquivo '
+	os.chdir('./' + directory) 
 	for file in ListFile:
 		f = open(file)
 		content = f.read()
 		if ContentSearch in content:
-			shutil.copy(file, 'path' ) # ex. '/home/exemplo/Desktop/pastaPrincipal'
+			shutil.copy(file, destination)
 			f.close()
-			exit()
+			print(f'\n!!! Achei no diretório {directory}!!!\n')
+				
+			
 
-os.chdir('path') # ex. '/home/exemplo/Desktop'
-folder = input('Nome da pasta principal que contém as pastas com os arquivos: ')
-directories = os.listdir(folder)
-ContentSearch = input('Conteúdo a ser procurado: ')
+				
 
-for directory in directories:
-	readFile(directory, ContentSearch)
+currentPath = input('Caminho onde se encontra a pasta principal (ex. "/home/exemplo/Desktop"): ')
+if currentPath == '00':
+	exit()
+else:
+	os.chdir(currentPath) 
+
+PrincipalFolder = input('Nome da pasta principal: ')
+if PrincipalFolder == '00':
+	exit()
+else:
+	directories = os.listdir(PrincipalFolder)
+
+destination = input('Destino onde a cópia do arquivo será movida: ')
+if destination == '00':
+	exit()	
+
+while True:
+	ContentSearch = input('Conteúdo a ser procurado: ')
+	if ContentSearch == '00':
+		exit()
+	else:
+		for directory in directories:
+			readFile(PrincipalFolder, directory, ContentSearch, destination)
 
 
 
